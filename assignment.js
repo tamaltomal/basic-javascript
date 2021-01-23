b7db73975a87f7e0fa5ab4bf026bc97aaf1262bd
tamaltomal <tamal.tomal@gmail.com>
2021-01-23 21:59:17 +0600

    done the fourth'

--- assignment.js
+++ assignment.js
@@ -6,9 +6,10 @@ function kilometerToMeter(km){
         return distance;
     }
     else{
-        return "Please enter a valid positive number.";
+        return "Please enter a valid positive number."; //catching invalid inputs
     }
 }
+
 
 function budgetCalculator(watch, phone, laptop){
     if(typeof(watch) == 'number' && watch > -1 && typeof(phone) == 'number' && phone > -1 && typeof(laptop) == 'number' && laptop > -1){
@@ -16,6 +17,49 @@ function budgetCalculator(watch, phone, laptop){
         return budget;
     }
     else{
-        return "Please enter three valid numbers seperated by commas.";
+        return "Please enter three valid numbers seperated by commas."; //catching invalid inputs
+    }
+}
+
+
+function hotelCost(days){
+    if(days >= 0 && days < 11){
+        let cost = days * 100; //first slot
+        return cost;
+    }
+    if(days >= 11 && days < 21){
+        let cost = (10 * 100) + ((days - 10) * 80); //second slot
+        return cost;
+    }
+    if(days > 20){
+        let cost = ((10 * 100) + (10 * 80) + ((days - 20) * 50)); //third slot
+        return cost;
+    }
+    else{
+        return "Please enter valid number of days."; //catching invalid inputs
+    }
+}
+
+
+function megaFriend(names){
+    if(names.length == 0){
+        return "You have no friend :-("; //Detecting unsocial person
+    }
+    else{
+        for(let i = 0; i < names.length; i++){
+            if(typeof(names[i]) != 'string'){
+                return "Please enter valid string as names."; //Detecting pakna person
+                break;
+            }
+        }
+        let nameSize = 0;
+        let gigaFriend;
+        for(let i = 0; i < names.length; i++){
+            if(nameSize < names[i].length){ //Comparing name sizes
+                nameSize = names[i].length;
+                gigaFriend = names[i]; //Storing larger names
+            }
+        }
+        return gigaFriend; //Returning the first occurence of largest names
     }
 }
