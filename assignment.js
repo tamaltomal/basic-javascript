7716adc0e986de488d54a95ce2031979f6c1d1e6
tamaltomal <tamal.tomal@gmail.com>
2021-01-23 19:15:07 +0600

    first two done

--- assignment.js
+++ assignment.js
@@ -1,1 +1,21 @@
 // https://github.com/tamaltomal/basic-javascript
+
+function kilometerToMeter(km){
+    if(typeof(km) == 'number' && km > -1){
+        let distance = km * 1000;
+        return distance;
+    }
+    else{
+        return "Please enter a valid positive number.";
+    }
+}
+
+function budgetCalculator(watch, phone, laptop){
+    if(typeof(watch) == 'number' && watch > -1 && typeof(phone) == 'number' && phone > -1 && typeof(laptop) == 'number' && laptop > -1){
+        budget = (watch * 50) + (phone * 100) + (laptop * 500);
+        return budget;
+    }
+    else{
+        return "Please enter three valid numbers seperated by commas.";
+    }
+}
